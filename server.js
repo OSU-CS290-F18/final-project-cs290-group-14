4ac3de34cca9691f015cd46b6f4f6d6f7ba1bb14
JoeVanKessel <vankessj@oregonstate.edu>
2018-12-06 04:51:07 -0800

    Added server/ created page add. NOTE: page add not done

---
+++ server.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.engine('handlebars', exphbs({deafaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+
+app.get('/',function(req,res){
+  res.status(200).render('home_page')
+});
+
+app.get('/addresume',function(req,res){
+  res.status(200).render('makeresume');
+});
+
+app.get('*', function (req, res) {
+  res.status(404).render('404Page');
+});
+
+app.listen(port, function () {
+  console.log("== Server is listening on port", port);
+});
