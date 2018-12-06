af35037a0b69a8870e68e4a5d299166fc1c39dfd
rc0664 <43710124+rc0664@users.noreply.github.com>
2018-12-06 06:37:08 -0800

    Update insertNewStudent.js

--- insertNewStudent.js
+++ insertNewStudent.js
@@ -12,3 +12,35 @@ var studentHTML = Handlebars.templates.studentInfo(studentData);
 var insertPostContainer = document.getElementById('students');
 insertPostContainer.insertAdjacentHTML('beforeend', postHTML);
 }
+var allStudents = [];
+
+function handleModalAcceptClick() {
+
+  var name = document.getElementById('user-name').value.trim();
+  var photo = document.getElementById('user-photo').value.trim();
+  var age = document.getElementById('user-age').value.trim();
+  var major = document.getElementById('user-major').value.trim();
+  var bio =  document.getElementById('user-bio').value.trim();
+  var resume = document.getElementById('user-resume');
+
+  if (!name || !age || !photo || !major || !bio || !resume) {
+    alert("You must fill in all of your information to create a post.");
+  } else {
+
+    allStudents.push({
+	name:name,
+	age:age,
+	photo:photo,
+	major:major,
+	bio:bio,
+      	resume:resume
+    });
+
+  }
+
+}
+
+var modalAcceptButton = document.getElementById('modal-accept');
+  if (modalAcceptButton) {
+    modalAcceptButton.addEventListener('click', handleModalAcceptClick);
+  }
