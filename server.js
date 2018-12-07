de153566e1c8a9c6046f9455f2244bf830d38646
rc0664 <43710124+rc0664@users.noreply.github.com>
2018-12-07 18:48:27 +0800

    relate it to mongodb

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
+var fs = require('fs');
 var path = require('path');
 var express = require('express');
-var app = express();
-var port = process.env.PORT || 3000;
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient;
+var studentData = require('./studentData');
+var methodOverride = require('method-override');
+
 app.use(bodyParser.json());
 app.use(express.static('public'));
+
+var mongoHost = process.env.MONGO_HOST || 'classmongo.engr.oregonstate.edu';
+var mongoPort = process.env.MONGO_PORT || '27017';
+var mongoUsername = process.env.MONGO_USERNAME || 'cs290_caiy';
+var mongoPassword = process.env.MONGO_PASSWORD || 'cs290_caiy';
+var mongoDBName = process.env.MONGO_DB_NAME || 'cs290_caiy';
+var mongoURL = "mongodb://" +
+		  mongoUsername + ":" + mongoPassword + "@" + mongoHost + ":" + mongoPort +
+		  "/" + mongoDBName;
+
+var mongoDB = null;
+
+var app = express();
+var port = process.env.PORT || 3000;
 
 app.engine('handlebars', exphbs({deafaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -34,7 +52,12 @@ app.get('/students', function(req, res, next) {
 	  }
 });
 
-
+MongoClient.connect(mongoURL, function (err, client) {
+	  if (err) {
+	    throw err;
+	  }
+	  mongoDB = client.db(mongoDBName);
 app.listen(port, function () {
   console.log("== Server is listening on port", port);
+	});
 });
