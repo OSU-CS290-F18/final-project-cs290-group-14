56e6b6f8867018997cfb52e11ffc783bb22b22de
rc0664 <43710124+rc0664@users.noreply.github.com>
2018-12-07 20:24:35 +0800

    Create (local)server.js

---
+++ (local)server.js
@@ -0,0 +1,44 @@
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var studentData = require('./studentData');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.engine('handlebars', exphbs({deafaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.get('/',function(req,res){
+  res.status(200).render('index')
+});
+
+app.get('/addresume',function(req,res){
+  res.status(200).render('addresume');
+});
+
+app.get('*', function (req, res) {
+  res.status(404).render('404');
+});
+
+app.get('/students', function(req, res, next) {
+	  var st = req.params.student.toLowerCase();;
+	  	if (studentData[st]) {
+	    res.render("partials/studentInfo", studentData[st]);
+		  console.log("single student page");
+	  }
+	  	else {
+	    next();
+	  }
+});
+
+
+app.listen(port, function () {
+  console.log("== Server is listening on port", port);
+});
